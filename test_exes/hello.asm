29d5d3ede788e1daf4ace56b71049ca5f656f085
David Fifield <david@bamsoftware.com>
2019-01-20 17:42:36 -0700

    Make hello.exe more elaborate, with relocations.

--- test_exes/hello.asm
+++ test_exes/hello.asm
@@ -3,14 +3,44 @@ BITS 16
 main:
 	mov ah, 0x40		; write to file handle
 	mov bx, 1		; stdout
-	mov cx, .msg_end-.msg	; length of string
+	mov cx, msg_len		; length of string
 	mov dx, cs
 	mov ds, dx
-	mov dx, .msg		; ds:dx points to string
-	int 0x21
-	mov ax, 0x4c00
+	mov dx, msg		; ds:dx points to string
 	int 0x21
 
-.msg:
-	db 'Hello, DOS'
-.msg_end:
+	mov si, numbers
+	mov cx, 4
+.loop:
+	mov ah, 0x02		; write character to stdout
+	mov dl, ' '		; character to write
+	int 0x21
+
+	xor bx, bx
+	mov bl, [si]
+	shr bx, 4		; high nibble
+	mov dl, [hexdigits+bx]
+	mov ah, 0x02
+	int 0x21
+
+	xor bx, bx
+	mov bl, [si]
+	and bx, 0xf		; low nibble 
+	mov dl, [hexdigits+bx]
+	mov ah, 0x02
+	int 0x21
+
+	inc si
+	loop .loop
+
+	mov ax, 0x4c00		; terminate with status 0
+	int 0x21
+
+numbers:
+	db	0x12, 0x34, 0xab, 0xcd
+
+hexdigits:
+	db "0123456789abcdef"
+msg:
+	db "Hello, DOS", 13, 10, "Lucky numbers"
+msg_len	equ	$ - msg
