viiri/exepack

commit dbf8eba5f09c82e3b60bfa3e470a708d4c9e40c3

Move exepack::Error into main.

src/main.rs
@@ -38,21 +38,64 @@ mod exe;
 mod exepack;
 mod pointer;
 
-struct TopLevelError {
-    path: Option<PathBuf>,
-    kind: exepack::Error,
+#[derive(Debug)]
+enum Error {
+    Io(io::Error),
+    Exe(exe::FormatError),
+    Exepack(exepack::FormatError),
 }
 
-impl fmt::Display for TopLevelError {
+impl std::error::Error for Error {}
+
+impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            TopLevelError { path: None, kind } => kind.fmt(f),
-            TopLevelError { path: Some(path), kind } => write!(f, "{}: {}", path.display(), kind),
+            Error::Io(err) => err.fmt(f),
+            Error::Exe(err) => err.fmt(f),
+            Error::Exepack(err) => err.fmt(f),
         }
     }
 }
 
-fn write_exe_file<P: AsRef<Path>>(path: P, exe: &exe::Exe) -> Result<u64, exepack::Error> {
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Error::Io(err)
+    }
+}
+
+impl From<exe::Error> for Error {
+    fn from(err: exe::Error) -> Self {
+        match err {
+            exe::Error::Io(err) => Error::Io(err),
+            exe::Error::Format(err) => Error::Exe(err),
+        }
+    }
+}
+
+impl From<exepack::FormatError> for Error {
+    fn from(err: exepack::FormatError) -> Self {
+        Error::Exepack(err)
+    }
+}
+
+#[derive(Debug)]
+struct TopLevelError {
+    path: Option<PathBuf>,
+    err: Error,
+}
+
+impl std::error::Error for TopLevelError {}
+
+impl fmt::Display for TopLevelError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TopLevelError { path: None, err } => err.fmt(f),
+            TopLevelError { path: Some(path), err } => write!(f, "{}: {}", path.display(), err),
+        }
+    }
+}
+
+fn write_exe_file<P: AsRef<Path>>(path: P, exe: &exe::Exe) -> Result<u64, Error> {
     let f = File::create(&path)?;
     let mut f = io::BufWriter::new(f);
     let n = exe.write(&mut f)?;
@@ -60,19 +103,20 @@ fn write_exe_file<P: AsRef<Path>>(path: P, exe: &exe::Exe) -> Result<u64, exepac
     Ok(n)
 }
 
-fn pack_file<P: AsRef<Path>>(path: P) -> Result<exe::Exe, exepack::Error> {
+fn pack_file<P: AsRef<Path>>(path: P) -> Result<exe::Exe, Error> {
     let f = File::open(&path)?;
     let file_len = f.metadata()?.len();
     let mut f = io::BufReader::new(f);
     let exe = exe::Exe::read(&mut f, Some(file_len))?;
-    exepack::pack(&exe)
+    exepack::pack(&exe)?;
+    Ok(exe)
 }
 
 fn compress_mode<P: AsRef<Path>>(input_path: P, output_path: P) -> Result<(), TopLevelError> {
     let exe = match pack_file(&input_path) {
         Err(err) => return Err(TopLevelError {
             path: Some(input_path.as_ref().to_path_buf()),
-            kind: err,
+            err: err,
         }),
         Ok(f) => f,
     };
@@ -80,14 +124,14 @@ fn compress_mode<P: AsRef<Path>>(input_path: P, output_path: P) -> Result<(), To
     if let Err(err) = write_exe_file(&output_path, &exe) {
         return Err(TopLevelError {
             path: Some(output_path.as_ref().to_path_buf()),
-            kind: err,
+            err: err,
         });
     }
 
     Ok(())
 }
 
-fn unpack_file<P: AsRef<Path>>(path: P) -> Result<exe::Exe, exepack::Error> {
+fn unpack_file<P: AsRef<Path>>(path: P) -> Result<exe::Exe, Error> {
     let f = File::open(&path)?;
     let file_len = f.metadata()?.len();
     let mut f = io::BufReader::new(f);
@@ -99,7 +143,7 @@ fn decompress_mode<P: AsRef<Path>>(input_path: P, output_path: P) -> Result<(), 
     let exe = match unpack_file(&input_path) {
         Err(err) => return Err(TopLevelError {
             path: Some(input_path.as_ref().to_path_buf()),
-            kind: err,
+            err: err,
         }),
         Ok(f) => f,
     };
@@ -107,7 +151,7 @@ fn decompress_mode<P: AsRef<Path>>(input_path: P, output_path: P) -> Result<(), 
     if let Err(err) = write_exe_file(&output_path, &exe) {
         return Err(TopLevelError {
             path: Some(output_path.as_ref().to_path_buf()),
-            kind: err,
+            err: err,
         });
     }
 
@@ -236,8 +280,8 @@ fn main() {
     } else {
         compress_mode(&input_path, &output_path)
     } {
-        match err.kind {
-            exepack::Error::Exepack(exepack::FormatError::UnknownStub(ref exepack_header_buffer, ref stub)) => {
+        match err.err {
+            Error::Exepack(exepack::FormatError::UnknownStub(ref exepack_header_buffer, ref stub)) => {
                 // UnknownStub gets special treatment. We search for "Packed
                 // file is corrupt" and display the stub if it is found, or warn
                 // that the input may not be EXEPACK if it is not.

src/exe.rs
@@ -29,6 +29,8 @@ pub enum Error {
     Format(FormatError),
 }
 
+impl std::error::Error for Error {}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match &self {

src/exepack.rs
@@ -60,44 +60,6 @@ fn push_u16le(buf: &mut Vec<u8>, v: u16) {
     buf.extend(&u16::to_le_bytes(v));
 }
 
-#[derive(Debug)]
-pub enum Error {
-    Io(io::Error),
-    Exe(exe::FormatError),
-    Exepack(FormatError),
-}
-
-impl From<io::Error> for Error {
-    fn from(err: io::Error) -> Self {
-        Error::Io(err)
-    }
-}
-
-impl From<exe::Error> for Error {
-    fn from(err: exe::Error) -> Self {
-        match err {
-            exe::Error::Io(err) => Error::Io(err),
-            exe::Error::Format(err) => Error::Exe(err),
-        }
-    }
-}
-
-impl From<FormatError> for Error {
-    fn from(err: FormatError) -> Self {
-        Error::Exepack(err)
-    }
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Error::Io(err) => err.fmt(f),
-            Error::Exe(err) => err.fmt(f),
-            Error::Exepack(err) => err.fmt(f),
-        }
-    }
-}
-
 #[derive(Debug, PartialEq)]
 pub enum FormatError {
     RelocationsNotSupported,
@@ -119,6 +81,8 @@ pub enum FormatError {
     CompressedTooLong(usize),
     SSTooLarge(usize),
 }
+
+impl std::error::Error for FormatError {}
 
 impl fmt::Display for FormatError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -409,7 +373,7 @@ fn compress(output: &mut Vec<u8>, input: &[u8]) {
 }
 
 /// Encode a compressed relocation table.
-fn encode_relocs(buf: &mut Vec<u8>, relocs: &[exe::Pointer]) -> Result<(), Error> {
+fn encode_relocs(buf: &mut Vec<u8>, relocs: &[exe::Pointer]) -> Result<(), FormatError> {
     // http://www.shikadi.net/moddingwiki/Microsoft_EXEPACK#Relocation_Table
     let mut relocs: Vec<_> = relocs.iter().cloned().collect();
     relocs.sort();
@@ -429,13 +393,13 @@ fn encode_relocs(buf: &mut Vec<u8>, relocs: &[exe::Pointer]) -> Result<(), Error
         i = j;
     }
     if i < relocs.len() {
-        return Err(Error::Exepack(FormatError::RelocationAddrTooLarge(relocs[i])));
+        return Err(FormatError::RelocationAddrTooLarge(relocs[i]));
     }
     Ok(())
 }
 
 /// Pack an input executable and return the elements of the packed executable.
-pub fn pack(exe: &exe::Exe) -> Result<exe::Exe, Error> {
+pub fn pack(exe: &exe::Exe) -> Result<exe::Exe, FormatError> {
     let mut uncompressed = exe.body.clone();
     // Pad uncompressed to a multiple of 16 bytes.
     {
@@ -472,11 +436,11 @@ pub fn pack(exe: &exe::Exe) -> Result<exe::Exe, Error> {
         real_ip: exe.e_ip,
         real_cs: exe.e_cs,
         exepack_size: exepack_size.try_into()
-            .or(Err(Error::Exepack(FormatError::ExepackTooLong(exepack_size))))?,
+            .or(Err(FormatError::ExepackTooLong(exepack_size)))?,
         real_sp: exe.e_sp,
         real_ss: exe.e_ss,
         dest_len: (uncompressed.len() / 16).try_into()
-            .or(Err(Error::Exepack(FormatError::UncompressedTooLong(uncompressed.len()))))?,
+            .or(Err(FormatError::UncompressedTooLong(uncompressed.len())))?,
         skip_len: 1,
         signature: EXEPACK_MAGIC,
     });
@@ -1085,7 +1049,7 @@ mod tests {
             exe.relocs.push(pointer);
             maybe_save_exe(format!("tests/reloc_{:04x}:{:04x}.exe", pointer.segment, pointer.offset), &exe).unwrap();
             match pack(&exe) {
-                Err(Error::Exepack(FormatError::RelocationAddrTooLarge(_))) => (),
+                Err(FormatError::RelocationAddrTooLarge(_)) => (),
                 x => panic!("{:?} {}", x, pointer),
             }
         }
@@ -1108,15 +1072,11 @@ mod tests {
         //   uncompressed size.
         // - the EXE header cs field needs to be greater than the compressed size.
 
-        // Check for an EXE or EXEPACK error in a macro, for meaningful line numbers
-        // in test output.
+        // Check for an error in a macro, for meaningful line numbers in test
+        // output.
         macro_rules! want_error {
             ($r:expr) => {
-                match $r {
-                    Err(Error::Exe(_)) => (),
-                    Err(Error::Exepack(_)) => (),
-                    x => panic!("{:?}", x),
-                }
+                assert!($r.is_err(), $r);
             }
         }
 
@@ -1281,7 +1241,7 @@ mod tests {
         pack(&exe).unwrap()
     }
 
-    fn save_exe<P: AsRef<path::Path>>(path: P, exe: &exe::Exe) -> Result<(), Error> {
+    fn save_exe<P: AsRef<path::Path>>(path: P, exe: &exe::Exe) -> Result<(), Box<dyn std::error::Error>> {
         let f = fs::File::create(path)?;
         let mut w = io::BufWriter::new(f);
         exe.write(&mut w)?;
@@ -1290,7 +1250,7 @@ mod tests {
     }
 
     // call save_exe if the environment variable EXEPACK_TEST_SAVE_EXE is set.
-    fn maybe_save_exe<P: AsRef<path::Path>>(path: P, exe: &exe::Exe) -> Result<(), Error> {
+    fn maybe_save_exe<P: AsRef<path::Path>>(path: P, exe: &exe::Exe) -> Result<(), Box<dyn std::error::Error>> {
         if let Some(_) = env::var_os("EXEPACK_TEST_SAVE_EXE") {
             save_exe(path, exe)?;
         }

commit 220b6b2730f5529c8aa4782448e930b217dc8316

Make compress mode work again.
It was returning the original EXE (after possibly truncating to the size
in the EXE header). This was broken in commit
dbf8eba5f09c82e3b60bfa3e470a708d4c9e40c3.

src/main.rs
@@ -109,8 +109,7 @@ fn pack_file<P: AsRef<Path>>(path: P) -> Result<exe::Exe, Error> {
     let file_len = f.metadata()?.len();
     let mut f = io::BufReader::new(f);
     let exe = exe::Exe::read(&mut f, Some(file_len))?;
-    exepack::pack(&exe)?;
-    Ok(exe)
+    exepack::pack(&exe).map_err(|err| From::from(err))
 }
 
 /// Calls `exepack::unpack` on the EXE file named by `path`.