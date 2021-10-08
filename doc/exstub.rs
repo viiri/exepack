73bbe8626c3d393b3d22fd1bce9706c93db25890
David Fifield <david@bamsoftware.com>
2021-10-08 15:34:31 -0600

    Enhance exstub.rs a bit.

--- doc/exstub.rs
+++ doc/exstub.rs
@@ -1,3 +1,5 @@
+//! Extracts the EXEPACK decompression stub from a file and writes it to stdout.
+
 extern crate exepack as exepack_crate;
 use exepack_crate::exe;
 
@@ -7,10 +9,19 @@ use std::io::{self, Write};
 use std::process;
 
 fn locate_end_of_stub(stub: &[u8]) -> Option<usize> {
-    const SUFFIX: &[u8] = b"Packed file is corrupt";
+    const SUFFIX: &[u8] = b"\xcd\x21\xb8\xff\x4c\xcd\x21";
+    const MESSAGE: &[u8] = b"Packed file is corrupt";
+    // First try looking for the error exit code as in the main exepack
+    // executable.
     for (i, window) in stub.windows(SUFFIX.len()).enumerate() {
         if window == SUFFIX {
-            return Some(i + SUFFIX.len());
+            return Some(i + SUFFIX.len() + MESSAGE.len());
+        }
+    }
+    // As a fallback, look for English "Packed file is corrupt".
+    for (i, window) in stub.windows(MESSAGE.len()).enumerate() {
+        if window == MESSAGE {
+            return Some(i + MESSAGE.len());
         }
     }
     None
@@ -21,7 +32,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = opts.parse(env::args().skip(1))?;
 
     if matches.free.len() != 1 {
-        eprintln!("Need INPUT.EXE argument");
+        eprintln!("usage: {} INPUT.EXE", env::args().next().unwrap());
         process::exit(1);
     }
     let path = &matches.free[0];
@@ -29,7 +40,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut f = File::open(&path)?;
     let exe = exe::Exe::read(&mut f, None)?;
     let stub = &exe.body[usize::from(exe.e_cs)*16 + usize::from(exe.e_ip)..];
-    let stub = &stub[..locate_end_of_stub(&stub).unwrap()];
+    let end = locate_end_of_stub(&stub).ok_or_else(|| "no EXEPACK decompression stub found")?;
+    let stub = &stub[..end];
     io::stdout().write(&stub)?;
 
     Ok(())
