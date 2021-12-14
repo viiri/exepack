60aadd8f2b9d277f2d40332056617131f2faf44e
David Fifield <david@bamsoftware.com>
2021-12-14 14:07:54 -0700

    Remove the unknown stub reporting feature.
    This seems rare. I'll let people see "unknown stub" and investigate
    themselves if they care to.

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,11 @@
 //! * <http://www.shikadi.net/moddingwiki/Microsoft_EXEPACK>.
 
 use std::env;
-use std::fmt::{self, Write as _};
+use std::fmt;
 use std::fs::File;
 use std::io::{self, Seek, Write};
 use std::path::{Path, PathBuf};
 use std::process;
-use std::str;
 
 extern crate exepack as exepack_crate;
 use exepack_crate::exe;
@@ -146,86 +145,6 @@ where
     Ok(())
 }
 
-/// A byte string found in most EXEPACK decompression stubs.
-const EXEPACK_ERRMSG: &[u8] = b"Packed file is corrupt";
-
-/// Returns whether the given slice contains `EXEPACK_ERRMSG`, and is therefore
-/// likely to be an EXEPACK decompression stub.
-fn stub_resembles_exepack(stub: &[u8]) -> bool {
-    // No equivalent of str::contains for &[u8]...
-    stub.windows(EXEPACK_ERRMSG.len()).any(|window| window == EXEPACK_ERRMSG)
-}
-
-#[test]
-fn test_stub_resembles_exepack() {
-    assert_eq!(stub_resembles_exepack(b""), false);
-    assert_eq!(stub_resembles_exepack(b"XXPacked XXPacked file is corrup"), false);
-    assert_eq!(stub_resembles_exepack(b"Packed file is corrupt"), true);
-    assert_eq!(stub_resembles_exepack(b"XXPacked file is corrupt"), true);
-    assert_eq!(stub_resembles_exepack(b"XXPacked file is corruptXXPacked file is corruptXX"), true);
-}
-
-/// Converts `buf` into a backslash-escaped ASCII-safe string without
-/// whitespace.
-fn escape(buf: &[u8]) -> String {
-    let mut s = String::new();
-    for &c in buf {
-        if c.is_ascii_alphanumeric() {
-            s.push(c.into())
-        } else {
-            write!(s, "\\x{:02x}", c).unwrap()
-        }
-    }
-    s
-}
-
-/// Writes `exepack_header_buffer` and `stub` to `w` in an ASCII-safe,
-/// reversible format.
-fn write_escaped_stub_for_submission<W: Write + ?Sized>(
-    w: &mut W,
-    exepack_header_buffer: &[u8],
-    stub: &[u8],
-) -> io::Result<()> {
-    let mut buf = Vec::new();
-    buf.extend_from_slice(exepack_header_buffer);
-    buf.extend_from_slice(stub);
-    for (i, chunk) in escape(&buf).as_bytes()
-        .chunks(64)
-        .enumerate()
-    {
-        writeln!(w, "={:02}={}==", i, str::from_utf8(chunk).unwrap())?;
-    }
-    Ok(())
-}
-
-/// Display an error message for an unknown decompression stub, including an
-/// invitation to submit it if it really appears to be EXEPACK.
-fn display_unknown_stub<W: Write + ?Sized>(
-    w: &mut W,
-    exepack_header_buffer: &[u8],
-    stub: &[u8],
-) -> io::Result<()> {
-    if stub_resembles_exepack(stub) {
-        write!(w, "\
-The input contains {:?}, but does not match a\n\
-format that this program knows about. Please send the below listing to\n\
-\tdavid@bamsoftware.com\n\
-ideally with a link to or copy of the executable, so that a future\n\
-version can support it.\n\
-\n",
-            str::from_utf8(EXEPACK_ERRMSG).unwrap()
-        )?;
-        write_escaped_stub_for_submission(w, exepack_header_buffer, stub)?;
-    } else {
-        write!(w, "\
-The input does not contain {:?} within the first\n\
-page of code. Is it really EXEPACK?\n",
-            str::from_utf8(EXEPACK_ERRMSG).unwrap()
-        )?;
-    }
-    Ok(())
-}
-
 /// Print a usage message to `w`.
 fn print_usage<W: Write + ?Sized>(w: &mut W, opts: getopts::Options) -> io::Result<()> {
     let brief = format!("\
@@ -274,16 +193,6 @@ fn main() {
 
     if let Err(err) = process(&input_path, &output_path, op) {
         eprintln!("{}", err);
-        match err.err {
-            Error::Exepack(exepack::FormatError::UnknownStub { ref exepack_header, ref stub }) => {
-                // UnknownStub gets special treatment. We search for "Packed
-                // file is corrupt" and display the stub if it is found, or warn
-                // that the input may not be EXEPACK if it is not.
-                eprintln!();
-                display_unknown_stub(&mut io::stderr(), &exepack_header, &stub).unwrap();
-            }
-            _ => (),
-        }
         process::exit(1);
     }
 }
